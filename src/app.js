4ebbabfe2fb51876ee7a3b4de0748682bb5e56a6
MichaelWiss <michael.wiss@gmail.com>
2017-09-26 12:13:58 -0500

    app.js reformating

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 console.log('App.js is running!');
 
-var template = <p>This is JSX from app.js! from Michael</p>;
+	var template = (
+	<div>
+	  <h1>This is JSX from app.js!</h1>
+	  <p>from Michael</p>
+	</div>
+	);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);

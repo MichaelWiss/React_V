4feac52864d6b9eb77a9c17122e7cfdcb35e010d
MichaelWiss <michael.wiss@gmail.com>
2017-10-03 14:32:00 -0500

    buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,75 +3,93 @@
 console.log('App.js is running!');
 
 var app = {
-			title: 'Indecision App',
-			subtitle: 'We help you help yourself',
-			options: ['One', 'Two']
+	title: 'Indecision App',
+	subtitle: 'We help you help yourself',
+	options: ['One', 'Two']
 };
 
 var template = React.createElement(
-			'div',
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		app.title
+	),
+	app.subtitle && React.createElement(
+		'p',
+		null,
+		app.subtitle
+	),
+	React.createElement(
+		'p',
+		null,
+		app.options.length > 0 ? 'Here are your options' : 'no options'
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
 			null,
-			React.createElement(
-						'h1',
-						null,
-						app.title
-			),
-			app.subtitle && React.createElement(
-						'p',
-						null,
-						app.subtitle
-			),
-			React.createElement(
-						'p',
-						null,
-						app.options.length > 0 ? 'Here are your options' : 'no options'
-			),
-			React.createElement(
-						'ol',
-						null,
-						React.createElement(
-									'li',
-									null,
-									'Item one'
-						),
-						React.createElement(
-									'li',
-									null,
-									'Item two'
-						),
-						React.createElement(
-									'li',
-									null,
-									'Item three'
-						),
-						React.createElement(
-									'li',
-									null,
-									'Item four'
-						)
-			)
+			'Item one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item two'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item three'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item four'
+		)
+	)
 );
 
 var count = 0;
 var addOne = function addOne() {
-			console.log('addOne');
+	console.log('addOne');
+};
+
+var minusOne = function minusOne() {
+	console.log('minusOne');
+};
+
+var reset = function reset() {
+	console.log('reset');
 };
 
 var templateTwo = React.createElement(
-			'div',
-			null,
-			React.createElement(
-						'h1',
-						null,
-						'Count: ',
-						count,
-						' '
-			),
-			React.createElement(
-						'button',
-						{ onClick: addOne },
-						'+1'
-			)
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Count: ',
+		count,
+		' '
+	),
+	React.createElement(
+		'button',
+		{ onClick: addOne },
+		'+1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: minusOne },
+		'-1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: reset },
+		'Reset'
+	)
 );
 
 var appRoot = document.getElementById('app');

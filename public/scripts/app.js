dcfe2f4d9b098631e6533494687a182aa8ced244
MichaelWiss <michael.wiss@gmail.com>
2017-10-02 19:29:45 -0500

    arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,91 +1,9 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running!');
-
-var app = {
-	title: 'Indecision App',
-	subtitle: 'We help you help yourself',
-	options: ['One', 'Two']
+var square = function square(x) {
+    return x * x;
 };
 
-var template = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		app.title
-	),
-	app.subtitle && React.createElement(
-		'p',
-		null,
-		app.subtitle
-	),
-	React.createElement(
-		'p',
-		null,
-		app.options.length > 0 ? 'Here are your options' : 'no options'
-	),
-	React.createElement(
-		'ol',
-		null,
-		React.createElement(
-			'li',
-			null,
-			'Item one'
-		),
-		React.createElement(
-			'li',
-			null,
-			'Item two'
-		),
-		React.createElement(
-			'li',
-			null,
-			'Item three'
-		),
-		React.createElement(
-			'li',
-			null,
-			'Item four'
-		)
-	)
-);
+var SquareArrow = function SquareArrow() {};
 
-var user = {
-	name: 'Michael',
-	age: 27,
-	location: 'New York'
-};
-
-function getLocation(location) {
-	if (location) {
-		return React.createElement(
-			'p',
-			null,
-			'Location: ',
-			location
-		);
-	}
-}
-
-var templateTwo = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		user.name ? user.name : 'Anonymous'
-	),
-	user.age && user.age >= 18 && React.createElement(
-		'p',
-		null,
-		'Age: ',
-		user.age
-	),
-	getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(square(8));

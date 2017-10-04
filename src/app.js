c57912911732bd8e0f74da835a1bc8042e69719e
MichaelWiss <michael.wiss@gmail.com>
2017-10-03 19:57:18 -0500

    render()

--- src/app.js
+++ src/app.js
@@ -4,20 +4,33 @@ console.log('App.js is running!');
  const app = {
    	title: 'Indecision App',
    	subtitle: 'We help you help yourself',
-   	options: ['One', 'Two']
+   	options: []
 };
 
 const onFormSubmit = (e) => {
   e.preventDefault();
 
   const option = e.target.elements.option.value;
+
+  if (option) {
+  	app.options.push(option);
+  	e.target.elements.option.value = '';
+  	render();
+  }
 };
 
-const template = (
-	  <div>
-	    <h1>{app.title}</h1>
-	    {app.subtitle && <p>{app.subtitle}</p>}
+
+	   
+const appRoot = document.getElementById('app');
+
+
+const render = () => {
+	const template = (
+	<div>
+	 <h1>{app.title}</h1>
+	 {app.subtitle && <p>{app.subtitle}</p>}
 	    <p>{app.options.length > 0 ? 'Here are your options' : 'no options'}</p>
+	    <p>{app.options.length}</p>
 	    <ol>
 	      <li>Item one</li>
 	      <li>Item two</li>
@@ -30,10 +43,11 @@ const template = (
 	    </form> 
 	  </div>
 	);
-const appRoot = document.getElementById('app');
 
+    ReactDOM.render(template, appRoot);
+};
 
-ReactDOM.render(template, appRoot);
+render();
 
 
 

1916a8dc31cef718e968fb93fdb9fa6cf50b89cb
MichaelWiss <michael.wiss@gmail.com>
2017-10-04 18:33:17 -0500

    classes

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,14 @@
+class Person {
+   constructor(name) {
+   	 this.name = name;
+   }
+}
+
+const me = new Person('Michael');
+console.log(me);
+
+const other = new Person('Antoinette');
+console.log(other);
+
+
+

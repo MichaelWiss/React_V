7b4b1c799ab634dc840fb7839a8539355bf1f64b
MichaelWiss <michael.wiss@gmail.com>
2017-10-05 13:31:50 -0500

    capitolize component class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -63,6 +63,70 @@ var Action = function (_React$Component2) {
 	return Action;
 }(React.Component);
 
+var Options = function (_React$Component3) {
+	_inherits(Options, _React$Component3);
+
+	function Options() {
+		_classCallCheck(this, Options);
+
+		return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).apply(this, arguments));
+	}
+
+	_createClass(Options, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Option 1'
+				),
+				React.createElement(
+					'h1',
+					null,
+					'Option 2'
+				),
+				React.createElement(
+					'h1',
+					null,
+					'Option 3'
+				)
+			);
+		}
+	}]);
+
+	return Options;
+}(React.Component);
+
+var AddOption = function (_React$Component4) {
+	_inherits(AddOption, _React$Component4);
+
+	function AddOption() {
+		_classCallCheck(this, AddOption);
+
+		return _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).apply(this, arguments));
+	}
+
+	_createClass(AddOption, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Add Options'
+				)
+			);
+		}
+	}]);
+
+	return AddOption;
+}(React.Component);
+
 var jsx = React.createElement(
 	'div',
 	null,
@@ -72,7 +136,9 @@ var jsx = React.createElement(
 		'Title'
 	),
 	React.createElement(Header, null),
-	React.createElement(Action, null)
+	React.createElement(Action, null),
+	React.createElement(Options, null),
+	React.createElement(AddOption, null)
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -15,11 +15,35 @@ class Action extends React.Component {
 	}
 }
 
+class Options extends React.Component {
+	render() {
+		return (
+         <div>
+           <h1>Option 1</h1>
+           <h1>Option 2</h1>
+           <h1>Option 3</h1>         
+         </div>
+		);
+	}
+}
+
+class AddOption extends React.Component {
+	render() {
+		return (
+			<div>
+			  <h1>Add Options</h1>
+			</div>
+		);
+	}
+} 
+
 const jsx = (
 	<div>
 	   <h1>Title</h1>
 	   <Header/>
 	   <Action />
+	   <Options />
+	   <AddOption />
 	</div>
 );
 

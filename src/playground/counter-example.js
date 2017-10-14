ced0a211872b66b58d6cebfe0902a4388f85e2c8
MichaelWiss <michael.wiss@gmail.com>
2017-10-14 16:28:06 -0500

    counter

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -8,6 +8,17 @@ class Counter extends React.Component {
          count: 0
     };
   }
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+  } 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
+  }
+
+
   handleAddOne() {
     this.setState((prevState) => {
        return {

db7e072494c93e4308821a284d1f1734cb3d40c0
MichaelWiss <michael.wiss@gmail.com>
2017-10-16 18:54:17 -0500

    react-dom

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
-//import './utils.js';
-// import subtract, { square, add, } from './utils.js';
-
-
-
-// console.log('app.js is running!!');
-
-// console.log(square(3));
-// console.log(add(1034, 27));
-// console.log(subtract(1345, 83));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(20));
-console.log(canDrink(13));
-console.log(isSenior(65));
+import React from 'react';
+import ReactDOM from 'react-dom';

d6713f7c513f0aee5a00d914272ca931bfa8a81e
MichaelWiss <michael.wiss@gmail.com>
2017-10-16 16:59:52 -0500

    exports

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import './utils.js'
+//import './utils.js';
+import { square, add } from './utils.js';
+
 
 
 console.log('app.js is running!!');
+
+console.log(square(3));
+console.log(add(1034, 27));

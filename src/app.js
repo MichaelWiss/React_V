15856b5ec874b200eb5c608874c6435318100e30
MichaelWiss <michael.wiss@gmail.com>
2017-10-17 21:03:23 -0500

    template

--- src/app.js
+++ src/app.js
@@ -3,4 +3,22 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+	return (
+     <div> 
+       <p>header</p>
+       {props.content}
+       <p>footer</p>
+     </div>		
+	);
+}
+
+const template = (
+	<div>
+	  <h1>Page Title</h1>
+	  <p>This is my page</p>
+	</div>  
+);
+
+	ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+	

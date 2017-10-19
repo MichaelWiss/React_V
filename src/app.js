29360b9acbbaf516ba15f159d1f4bea6b6ab7539
MichaelWiss <michael.wiss@gmail.com>
2017-10-19 13:39:47 -0500

    styles

--- src/app.js
+++ src/app.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-import './styles/styles.css';
+import './styles/styles.scss';
 
-
-const Layout = (props) => {
-	return (
-     <div> 
-       <p>header</p>
-       {props.content}
-       <p>footer</p>
-     </div>		
-	);
-}
-
-const template = (
-	<div>
-	  <h1>Page Title</h1>
-	  <p>This is my page</p>
-	</div>  
-);
-
-	ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
 	

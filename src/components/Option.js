de4cdd0ea1621aefb9face8b5ccf2ac70370bd4b
MichaelWiss <michael.wiss@gmail.com>
2017-10-21 14:59:01 -0500

    pass index

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Option = (props) =>  (
-        <div>
-           {props.optionText}
+       <div className="option">
+        <p>{props.count}. {props.optionText}</p> 
            <button 
             className="button button--link" 
             onClick={(e) => {

407ee8021fb619a0fac4cc1efeb32bb51192cd09
MichaelWiss <michael.wiss@gmail.com>
2017-10-23 17:38:12 -0500

    edit props

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
-
+import Header from '../components/Header';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
-const Header = () => (
-	<header>
-	   <h1>Expensify</h1>
-	   <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-	   <NavLink to="/create" activeClassName="is-active">Add an expense</NavLink>
-	   <NavLink to="/edit" activeClassName="is-active">Edit expenses</NavLink>
-	   <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-	</header>
-);
+
 
 const AppRouter = () => (
    <BrowserRouter>
@@ -25,7 +17,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
      </Switch>

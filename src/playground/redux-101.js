614405ad8662da37b0b2745e7746248ad7ece76e
MichaelWiss <michael.wiss@gmail.com>
2017-10-24 15:11:29 -0500

    switch statement

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,6 +2,27 @@ import { createStore } from 'redux';
 
 
 
-const store = createStore((state) => {
-
+const store = createStore((state = {count: 0 }, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+		  return {
+      count: state.count + 1
+    };
+    default:
+      return state;
+  }
 });
+
+console.log(store.getState());
+
+//increment
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+
+console.log(store.getState());

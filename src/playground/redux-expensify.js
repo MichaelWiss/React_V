a2398aab9005e62336250b777b45d463988510f3
MichaelWiss <michael.wiss@gmail.com>
2017-10-29 17:05:12 -0500

    redux-expensify

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState ={
+   expenses: [{
+   	id:'kkhdfhoajfoa',
+   	description: 'January rent',
+   	note: 'This was the final payment for the address',
+   	amount: 54500,
+   	createdAt: 0
+   }]
+};

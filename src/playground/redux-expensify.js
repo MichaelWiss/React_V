eb609bb78241649d61086a32399a31c6255b3eee
MichaelWiss <michael.wiss@gmail.com>
2017-11-01 14:47:53 -0500

    expensesReducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,16 @@
 import { createStore, combineReducers } from 'redux';
 
+//expenses reducer
+
+const expensesReducerDefaultState = []; 
+
+const expensesReducer = (expensesReducerDefaultState , action) => {
+     switch (action.type) {
+        default:
+           return state;
+     }   
+};
+
 const demoState ={
    expenses: [{
    	id:'kkhdfhoajfoa',

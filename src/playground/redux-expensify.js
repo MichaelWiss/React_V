d7dd167e3c84aeb2d635dbace2cf2ac285d95345
MichaelWiss <michael.wiss@gmail.com>
2017-11-08 20:42:38 -0600

    expense edit function

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -42,7 +42,17 @@ const expensesReducer = (state = expensesReducerDefaultState , action) => {
          ];
          case 'REMOVE_EXPENSE':
           return state.filter(({id}) => id !== action.id);
-         case  
+         case 'EDIT_EXPENSE':
+          return state.map((expense) => {
+            if(expense.id === action.id) {
+              return {
+                ...expense,
+                ...action.updates
+              };
+            } else {
+               return expense;
+            }
+          }); 
         default:
            return state;
      }   

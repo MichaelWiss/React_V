0205820baee135ec477f1e57c76d6303c2f53396
MichaelWiss <michael.wiss@gmail.com>
2017-11-14 15:33:18 -0600

    new expenses

--- src/app.js
+++ src/app.js
@@ -18,9 +18,13 @@ const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount
 
 const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 40, createdAt: 1000 }));
 
-store.dispatch(setTextFilter('bill', 'water'));
+store.dispatch(setTextFilter('water'));
 
-console.log(store.getState());
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 	

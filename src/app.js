0b45bc0f6036b174c9b2fd724683e9a77e76809f
MichaelWiss <michael.wiss@gmail.com>
2017-11-14 15:26:33 -0600

    bils

--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 const store = configureStore();
+
+//water bill
+//gas bill
+
+const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 70, createdAt: 1200 }));
+
+const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', amount: 40, createdAt: 1000 }));
+
+store.dispatch(setTextFilter('bill', 'water'));
 
 console.log(store.getState());
 

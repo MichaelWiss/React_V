0ecf5a904ebfe9129df4667d07f2d1746d42c018
MichaelWiss <michael.wiss@gmail.com>
2017-11-17 17:15:19 -0600

    hoc

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,11 +1,29 @@
+//higher order component
+//reuse code
+//render hijacking
+//prop manipulation
+//abstract state
 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const info = (props) => (
+const Info = (props) => (
    <div>
      <h1>info</h1>
      <p>The info is: {props.info}</p>
    </div> 
 );
+
+const withAdminWarning = (WrappedComponent) => {
+   return (props) => (
+   	  <div>
+   	  <p>This is private info please don't share</p>
+   	  <WrappedComponent />
+   	  </div>
+   	);
+};
+
+const AdminInfo = withAdminWarning(Info);
+
+ReactDOM.render(<AdminInfo info="These are the details" />, document.getElementById('app'));

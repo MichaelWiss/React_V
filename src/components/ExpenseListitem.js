f54fa096be1da1cfecc368fa34e4464adcd977b5
MichaelWiss <michael.wiss@gmail.com>
2017-11-27 16:53:12 -0600

    remove bug

--- src/components/ExpenseListitem.js
+++ src/components/ExpenseListitem.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 
 const ExpenseListItem = ({ description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
-    <button>Remove</button>
+    <button onChange={(e) => {
+	  	props.dispatch(removeExpense(e.target.value));
+	  }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect(removeExpense)(ExpenseListItem);

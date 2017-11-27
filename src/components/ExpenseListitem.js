e255d64f47f24e36dc45bcdce13f985d3b7a8494
MichaelWiss <michael.wiss@gmail.com>
2017-11-27 17:08:06 -0600

    remove

--- src/components/ExpenseListitem.js
+++ src/components/ExpenseListitem.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
-    <button onChange={(e) => {
-	  	props.dispatch(removeExpense(e.target.value));
+    <button onClick={(e) => {
+	    dispatch(removeExpense({ id }));
 	  }}>Remove</button>
   </div>
 );
 
-export default connect(removeExpense)(ExpenseListItem);
+export default connect()(ExpenseListItem);

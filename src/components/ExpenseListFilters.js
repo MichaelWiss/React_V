c3f8cf40630e6d3e59173c93f83f2a2465386456
MichaelWiss <michael.wiss@gmail.com>
2017-11-28 17:24:47 -0600

    sortBy working

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -13,8 +13,8 @@ const ExpenseListFilters = (props) => (
 	  />
 	  <select 
 	    value={props.filters.sortBy}
-        onChange{(e) => {
-        	if (e.target.value === 'amount') {
+        onChange={(e) => {
+        	if (e.target.value === 'date') {
               props.dispatch(sortByDate());
         	} else if (e.target.value === 'amount') {
               props.dispatch(sortByAmount());

7c4bcded7d37e949d44c6516fdb738f61563681e
MichaelWiss <michael.wiss@gmail.com>
2017-11-28 18:53:04 -0600

    form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 const AddExpensePage = () => (
 	  <div>
 	    <h1>Add Expense</h1>
-	    <ExpenseForm/>
+	    <ExpenseForm />
 	 </div>
 );
 

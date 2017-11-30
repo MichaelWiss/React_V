87e866daef8045407f18bfd5231c8f07c60bd485
MichaelWiss <michael.wiss@gmail.com>
2017-11-30 14:23:16 -0600

    form tweaks

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -26,12 +26,14 @@ export default class ExpenseForm extends React.Component {
 	onAmountChange = (e) => {
 		const amount = e.target.value;
 
-		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+		if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
 		this.setState(() => ({ amount })); 
 		}
 	};
 	onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt}));
+		if (dreatedAt) {
+		 this.setState(() => ({ createdAt}));	
+		}
       };
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calenderFocused: focused }));
@@ -40,7 +42,7 @@ export default class ExpenseForm extends React.Component {
 	render() {
 		return (
          <div>
-           <form>
+           <form onSubmit=>
             <input
               type="text"
               placeholder="Description"
@@ -59,6 +61,8 @@ export default class ExpenseForm extends React.Component {
               onDateChange={this.onDateChange}
               focused={this.state.calenderFocused}
               onFocusChange={this.onFocusChange}
+              numberOfMonths={1}
+              isOutsideRange={() => false}
             />
             <textarea
              placeholder="add a note for your expense (optional)"

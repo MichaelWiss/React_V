b4ee3360b64b30768d3334b7a0865af0d1273fcc
MichaelWiss <michael.wiss@gmail.com>
2017-11-30 13:30:28 -0600

    dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
 
 //const date = new Date();
 const now = moment();
-console.log(now.format());
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
 	state = {

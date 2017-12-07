2c7b8d70d9b9b6b681503cb0a006e3d267623cff
MichaelWiss <michael.wiss@gmail.com>
2017-12-06 18:05:38 -0600

    missplelled

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-router';
+import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
 const EditExpensePage = (props) => {

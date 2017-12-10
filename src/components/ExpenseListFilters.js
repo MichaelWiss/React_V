a1b64b19d00ef8bced3d7d7c358f0f0f0fab671d
MichaelWiss <michael.wiss@gmail.com>
2017-12-09 20:25:24 -0600

    onFocusChange

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
-import { setTextFilter, sortByDate, sortByAmount  } from '../actions/filters';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate  } from '../actions/filters';
 
 class ExpenseListFilters extends React.Component {
 	state = {
        calendarFocused: null
 	};
 	onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+	};
+	onFocusChange = () => {
 
 	};
 	render () {
@@ -37,6 +41,8 @@ class ExpenseListFilters extends React.Component {
 	    startDate={this.props.filters.startDate}
 	    endDate={this.props.filters.endDate}
 	    onDatesChange={this.onDatesChange}
+	    focusedInput={this.state.calendarFocused}
+	    onFocusChange={this.onFocusChange}
       />
 	</div>  
    );

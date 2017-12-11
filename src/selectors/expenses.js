7e3ab0408e9aa101da7b67f4dfa3ad97bdf5b805
MichaelWiss <michael.wiss@gmail.com>
2017-12-10 19:21:24 -0600

    moment methods

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,8 @@
 //get visible expenses
 export default (expenses, { text, sortBy, startDate, endDate }) => {
    return expenses.filter((expense) => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const startDateMatch = 
+        const endDateMatch = 
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;

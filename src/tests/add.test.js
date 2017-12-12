9fac34414e0aa16cffdb1105dc9e63272b7d8fde
MichaelWiss <michael.wiss@gmail.com>
2017-12-11 18:31:06 -0600

    greeting

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,12 @@
 const add = (a, b) => a + b;
+const generatedGreeting = (name) => `Hello ${name}!`;
 
 test('should add two numbers', () => {
 	const result = add(3, 4);
 	expect(result).toBe(7);
+});
+
+test('should return a greeting', () => {
+	const reply = generatedGreeting('Mike');
+	expect (reply).toBe('Hello Mike!'); 
 });

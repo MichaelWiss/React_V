eebf6a56f91590fb614ee9244a7a69a9b7e204e6
MichaelWiss <michael.wiss@gmail.com>
2017-12-13 13:25:57 -0600

    filepath issue

--- src/tests/expenses.test.js
+++ src/tests/expenses.test.js
@@ -1,5 +1,20 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { addExpense, editExpense, removeExpense } from '../actions/expenses';
 
 test('should setup remove expense action object', () => {
    const action = removeExpense({ id: '123abc' });
+   expect(action).toEqual({
+       type: 'REMOVE_EXPENSE',
+       id: '123abc'
+   });
+});
+
+test('should setup editExpense action object', () => {
+	const action = editExpense('123abc', { note: 'new note value' });
+	expect(action).toEqual({
+		type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+        	note: 'New note value'
+        }
+	});
 });

682699485472f5d0a0bc4fe73c97dc28a5f16107
MichaelWiss <michael.wiss@gmail.com>
2017-12-28 04:32:18 -0600

    expenses array

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,5 +1,25 @@
 import selectExpenses from '../../selectors/expenses';
 
+const expenses = [{
+     id: '1',
+     description: 'Gum',
+     note: '',
+     amount: 195,
+     createdAt: 0
+}, {
+	 id: '2',
+     description: 'Rent',
+     note: '',
+     amount: 109500,
+     createdAt: -1000
+}, {
+	 id: '3',
+     description: 'Credit Card',
+     note: '',
+     amount: 4500,
+     createdAt: 1000
+}]
+
 test('should filter by test value', () => {
-   const result = selectExpenses();
+   const result = selectExpenses(expenses);
 });

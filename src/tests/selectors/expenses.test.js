4c9cfaa370ca85ba18cc2353ce5548d1f2c3d692
MichaelWiss <michael.wiss@gmail.com>
2017-12-28 05:49:16 -0600

    tests pass

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -22,7 +22,7 @@ const expenses = [{
 }];
 
 
-test('should filter by test value', () => {
+test('should filter by text value', () => {
 	const filters = {
 		text: 'e',
 		sortBy: 'date',
@@ -42,6 +42,39 @@ test('should filter by startDate', () => {
 	};
    const result = selectExpenses(expenses, filters);
    expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test('should filter by endDate', () => {
+     const filters = {
+		text: '',
+		sortBy: 'date',
+		startDate: undefined,
+		endDate: moment(0).add(2, 'days')
+	};
+   const result = selectExpenses(expenses, filters);
+   expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test('should sort by date', () => {
+     const filters = {
+		text: '',
+		sortBy: 'date',
+		startDate: undefined,
+		endDate: undefined
+	};
+   const result = selectExpenses(expenses, filters);
+   expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test('should sort by amount', () => {
+    const filters = {
+		text: '',
+		sortBy: 'amount',
+		startDate: undefined,
+		endDate: undefined
+	};
+   const result = selectExpenses(expenses, filters);
+   expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
 });
 
 

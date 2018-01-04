009c34b3a324cf5fb3de7a43e44c306cdd6a9910
MichaelWiss <michael.wiss@gmail.com>
2018-01-04 15:59:10 -0600

    test

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,1 +1,6 @@
 import filtersReducer from '../../reducers/filters';
+
+test('should set up default filter values', () => {
+    const state = filterReducer(undefined, { type: '@@INIT' });
+    
+});

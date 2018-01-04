35ae4c0773ae59748c450c10f853bd7e3814a7c2
MichaelWiss <michael.wiss@gmail.com>
2018-01-04 16:16:52 -0600

    test

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -2,5 +2,8 @@ import filtersReducer from '../../reducers/filters';
 
 test('should set up default filter values', () => {
     const state = filterReducer(undefined, { type: '@@INIT' });
-    
+    expect(state).toEqual({
+       text: '',
+       sortBy: 'date',
+    });
 });

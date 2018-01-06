a6c7498b349cec5e28670721d43fac9ecb03b285
MichaelWiss <michael.wiss@gmail.com>
2018-01-06 15:56:11 -0600

    testing

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import filtersReducer from '../../reducers/filters';
 
 test('should set up default filter values', () => {
@@ -8,4 +9,21 @@ test('should set up default filter values', () => {
        startDate: moment().startOf('month'),
        endDate: moment().endOf('month')
     });
+});
+
+test('should set sortBy to amount', () => {
+   const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+   expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', () => {
+  const currentState = {
+    text: '',
+    startDate: undefined,
+    endDate: undefined,
+    sortBy: 'amount'
+  };
+  const action = { type: 'SORT_BY_DATE' };
+  const state = filtersReducer(currentState, action);
+  expect(state.sortBy).toBe('date');
 });

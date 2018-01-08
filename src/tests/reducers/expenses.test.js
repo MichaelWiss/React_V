30f20c690356c636f1782bfc9e0f03dcccfd0371
MichaelWiss <michael.wiss@gmail.com>
2018-01-08 08:16:19 -0600

    fixture

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,6 +1,8 @@
 import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
 
 test('should set default star', () => {
    const state = expensesReducer(undefined, { type: '@@INIT' });
    expect(state).toEqual([]);
 });
+

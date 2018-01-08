04f9fbe4058385fe8976afc6d4142d88710f9b32
MichaelWiss <michael.wiss@gmail.com>
2018-01-08 07:42:45 -0600

    test

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,1 +1,6 @@
-import expensesReducer from '../../'
+import expensesReducer from '../../reducers/expenses';
+
+test('should set default star', () => {
+   const state = expensesReducer(undefined, { type: '@@INIT' });
+   expect(state).toEqual([]);
+});

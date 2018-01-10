2052177019bad0d8eb329e4213c80031c1058ac0
MichaelWiss <michael.wiss@gmail.com>
2018-01-10 11:09:50 -0600

    render Header

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
+
+test('should render header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header/>);
+    console.log(renderer.getRenderOutput());
+});

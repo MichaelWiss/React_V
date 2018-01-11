c4b30c7e5336fd320368ab7b9ac41c3f44a5db08
MichaelWiss <michael.wiss@gmail.com>
2018-01-11 12:14:10 -0600

    testing:

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,7 +5,7 @@ import Header from '../../components/Header';
 
 test('should render header correctly', () => {
 	const wrapper = shallow(<Header />);
-	expect(wrapper.find('h1').length).toBe(1);
+	expect(wrapper.find('h1').text()).toBe('Expensify');
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header/>);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();

799ed03de3902a912552a5b1f471a160ed808031
MichaelWiss <michael.wiss@gmail.com>
2018-01-11 12:38:41 -0600

    teating config

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 
 test('should render header correctly', () => {
 	const wrapper = shallow(<Header />);
-	expect(toJSON(wrapper)).toMatchSnapshot(); 
+	expect(wrapper).toMatchSnapshot(); 
 	// expect(wrapper.find('h1').text()).toBe('Expensify');
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header/>);

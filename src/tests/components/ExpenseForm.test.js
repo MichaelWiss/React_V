e2ef3b12a8481e677edc4c4291f2a2375207f256
MichaelWiss <michael.wiss@gmail.com>
2018-01-13 13:51:30 -0600

    expenseForm test

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -3,5 +3,6 @@ import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 
 test('should render ExpenseForm correctly', () => {
-
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot(); 
 });

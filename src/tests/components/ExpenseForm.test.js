739bb367f562d4079c5e38d2089951b24f4dc231
MichaelWiss <michael.wiss@gmail.com>
2018-01-13 14:21:11 -0600

    tesst for invalid form submission

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,7 @@ test('should render ExpenseForm correctly with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[1]}/>);
     expect(wrapper).toMatchSnapshot(); 
 });
+
+test)'should render error for invalid form submission', ()=> {
+
+};

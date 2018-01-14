da84e245ab94ae18136b3402e0f9e46190d618d0
MichaelWiss <michael.wiss@gmail.com>
2018-01-14 12:39:57 -0600

    failing test

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -14,11 +14,11 @@ test('should render ExpenseForm correctly with expense data', () => {
 });
 
 test('should render error for invalid form submission', () => {
-    const wrapper = shallow(<ExpenseForm />);
-    expect(wrapper).toMatchSnapshot();
-    wrapper.find('form').simulate('submit', {
-    	preventDefault: () => { }
-    });
-    expect(wrapper.state('error').length).toBeGreaterThan(0);
-    expect(wrapper).toMatchSnapshot();
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
 });

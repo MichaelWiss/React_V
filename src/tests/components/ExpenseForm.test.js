e5b4ebb42927419dd8a6f0eb071a8a2e0f209d50
MichaelWiss <michael.wiss@gmail.com>
2018-01-17 16:20:41 -0600

    cannot read state

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -4,13 +4,13 @@ import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
 test('should render ExpenseForm correctly', () => {
-    const wrapper = shallow(<ExpenseForm />);
-    expect(wrapper).toMatchSnapshot(); 
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should render ExpenseForm correctly with expense data', () => {
-    const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
-    expect(wrapper).toMatchSnapshot(); 
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should render error for invalid form submission', () => {
@@ -23,8 +23,8 @@ test('should render error for invalid form submission', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-test('shoud set description on input change', () => {
-  const value = 'New description';	
+test('should set description on input change', () => {
+  const value = 'New description';
   const wrapper = shallow(<ExpenseForm />);
   wrapper.find('input').at(0).simulate('change', {
     target: { value }

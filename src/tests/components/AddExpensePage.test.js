8234c57cdb2b8223fbfb0facb457f95ec0c01bce
MichaelWiss <michael.wiss@gmail.com>
2018-01-21 15:03:25 -0600

    testing

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,5 +3,8 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 
 test('should render AddExpensePage correctly', () => {
-    
+    const onSubmit = jest.fn();
+    const history  = { push: jest.fn(); };
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    	
 });

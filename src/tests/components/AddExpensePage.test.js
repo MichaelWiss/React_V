cd9a76cef28a3a2b7aac070cd9daffa28a33ec94
MichaelWiss <michael.wiss@gmail.com>
2018-01-21 15:49:00 -0600

    test globals

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,23 +5,17 @@ import expenses from'../fixtures/expenses'
 
 let onSubmit, history, wrapper;
 
-beforeEach() => {
-    const onSubmit = jest.fn();
-    const history  = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+beforeEach(() => {
+    onSubmit = jest.fn();
+    history  = { push: jest.fn() };
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
 });
 
 test('should render AddExpensePage correctly', () => {
-    const onSubmit = jest.fn();
-    const history  = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-	const onSubmit = jest.fn();
-    const history  = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenCalledWith('/');
     expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);

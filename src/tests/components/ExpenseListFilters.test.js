262378931e846cab1dfe663ae388f31f9b251ccc
MichaelWiss <michael.wiss@gmail.com>
2018-01-23 16:06:45 -0600

    import filters

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';

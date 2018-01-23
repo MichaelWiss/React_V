d0bc01a4a52a48e53448b3c02550dedd75cc7c37
MichaelWiss <michael.wiss@gmail.com>
2018-01-22 18:44:31 -0600

    test

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import { EditExpensePage } from '../../components/EditExpensePage';
+
+let editExpense, removeExpense, history, wrapper;
+
+beforeEach(() => {
+    editExpense = jest.fn();
+    removeExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<EditExpensePage/>);
+});

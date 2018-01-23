5376f3fb9d13f8b7c39357d51313c7a8954f253c
MichaelWiss <michael.wiss@gmail.com>
2018-01-23 16:14:28 -0600

    filter testing

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,3 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+   setTextFilter = jest.fn();
+   sortByDate = jest.fn();
+   sortByAmount = jest.fn();
+   setStartDate = jest.fn();
+   setEndDate= jest.fn();
+   wrapper = shallow(
+   	<ExpenseListFilters
+       filters={filters}
+       setTextFilters={setTextFilter}
+       sortByDate={sortByDate}
+       sortByAmount={sortByAmount}
+       setStartDate={setStartDate}
+       setEndDate={setEndDate}      
+   	/>
+   	)
+});

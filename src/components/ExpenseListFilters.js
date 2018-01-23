b2d6ea75dcd3f69423db1a1dbbe911ad17e97bc2
MichaelWiss <michael.wiss@gmail.com>
2018-01-23 14:11:08 -0600

    onTextChange

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -14,15 +14,16 @@ class ExpenseListFilters extends React.Component {
 	onFocusChange = (calendarFocused) => {
        this.setState(() => ({ calendarFocused }));
 	}
+	onTextChange = (e) => {
+	  	this.props.dispatch(setTextFilter(e.target.value));
+	  };
 	render () {
 	return (
 	 <div>
 	  <input 
 	  type="text" 
 	  value={this.props.filters.text} 
-	  onChange={(e) => {
-	  	this.props.dispatch(setTextFilter(e.target.value));
-	  }} 
+	  onChange={this.onTextChange} 
 	  />
 	  <select 
 	    value={this.props.filters.sortBy}

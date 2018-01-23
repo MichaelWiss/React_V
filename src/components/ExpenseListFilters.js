d5fe02ee98288a2058b9218daa1b29b095d2343c
MichaelWiss <michael.wiss@gmail.com>
2018-01-23 15:34:43 -0600

    refactor for testing

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -8,20 +8,20 @@ class ExpenseListFilters extends React.Component {
        calendarFocused: null
 	};
 	onDatesChange = ({ startDate, endDate }) => {
-        this.props.dispatch(setStartDate(startDate));
-        this.props.dispatch(setEndDate(endDate));
+        this.props.setStartDate(startDate);
+        this.props.setEndDate(endDate);
 	};
 	onFocusChange = (calendarFocused) => {
        this.setState(() => ({ calendarFocused }));
 	}
 	onTextChange = (e) => {
-	  	this.props.dispatch(setTextFilter(e.target.value));
+	  	this.props.setTextFilter(e.target.value);
 	  };
 	onSortChange = (e) => {
         	if (e.target.value === 'date') {
-               this.props.dispatch(sortByDate());
+               this.props.sortByDate();
         	} else if (e.target.value === 'amount') {
-              this.props.dispatch(sortByAmount());
+              this.props.sortByAmount();
         }
      };   
 	render () {

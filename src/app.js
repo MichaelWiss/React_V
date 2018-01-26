9100da8a6e6857fb75eecdbd46d28e0a3513e772
MichaelWiss <michael.wiss@gmail.com>
2018-01-26 15:02:37 -0600

    preparing for express

--- src/app.js
+++ src/app.js
@@ -13,16 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-//water bill
-//gas bill
-
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
 	<Provider store={store}>
       <AppRouter />

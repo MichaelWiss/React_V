a3a140cf9306d74a38f9b23ea141f89a16969210
MichaelWiss <michael.wiss@gmail.com>
2018-02-01 12:06:23 -0600

    express server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+
+});
+
 app.listen(3000, ()=> {
 	console.log('server is up');
 });

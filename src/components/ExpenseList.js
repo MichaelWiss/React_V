782ceb84950c8264c94ed41427c644c1808feb93
MichaelWiss <michael.wiss@gmail.com>
2018-02-08 14:29:03 -0600

    expenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
-import ExpenseListItem from './ExpenseListItem';
 
 
 export const ExpenseList = (props) => (

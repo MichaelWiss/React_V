71d022dea912139641fcd2b81fb0ffe1f3df6e0a
MichaelWiss <michael.wiss@gmail.com>
2018-02-20 17:18:25 -0600

    testing

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseSummary } from '../../components/ExpensesSummary';
 
-test('', () => {
+test('should correctly render ExpensesSummary with 1 expense', () => {
 
 });
 
-test('', () => {
+test('should correctly render ExpensesSummary with multiple expenses', () => {
 
 });

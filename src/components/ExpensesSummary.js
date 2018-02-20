68b8a66e154c4a5cb731d4359dd474285491493b
MichaelWiss <michael.wiss@gmail.com>
2018-02-20 17:09:38 -0600

    expensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import numeral from 'numeral';
 
 const ExpenseSummary = ({ expenseCoun, expenseTotal }) => {
+	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
    return (
    	<div>
-   	  <h1>Viewing {expenseCount} totaling
+   	  <h1>Viewing {expenseCount} expenses totaling $12.94</h1>
    	</div>
    	)
 };

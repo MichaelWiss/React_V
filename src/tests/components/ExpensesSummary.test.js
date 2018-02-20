d1e76e8b4049a71afd516d023288368543d11530
MichaelWiss <michael.wiss@gmail.com>
2018-02-20 17:39:16 -0600

    tdd

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,7 +3,8 @@ import { shallow } from 'enzyme';
 import { ExpenseSummary } from '../../components/ExpensesSummary';
 
 test('should correctly render ExpensesSummary with 1 expense', () => {
-
+   const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} /);
+   	expect(wrapper).toMatchSnapshot();
 });
 
 test('should correctly render ExpensesSummary with multiple expenses', () => {

56f9390c92b0fd412335333bbbc8b92560a50904
MichaelWiss <michael.wiss@gmail.com>
2018-02-20 18:28:58 -0600

    export const ExpensesSumary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 
-export const ExpenseSummary = ({ expenseCoun, expenseTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
-	const formattedExpensesTotal = numeral(expenseTotal / 100).format('$0,0.00');
+	const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
    return (
    	<div>
-   	  <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+   	  <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
    	</div>
-   	)
+   	);
 };
+
+
+

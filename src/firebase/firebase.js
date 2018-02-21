07e5bfc072488b3148a2d2bde09edf3a8be42a92
MichaelWiss <michael.wiss@gmail.com>
2018-02-21 14:36:21 -0600

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+ const config = {
+    apiKey: "AIzaSyB5CThmCjH690y293sa9WkORR6A7tdCz6w",
+    authDomain: "expensify-wiss.firebaseapp.com",
+    databaseURL: "https://expensify-wiss.firebaseio.com",
+    projectId: "expensify-wiss",
+    storageBucket: "expensify-wiss.appspot.com",
+    messagingSenderId: "134262336316"
+  };
+
+  firebase.initializeApp(config);
+
+  firebase.database().ref().set({
+     name: 'Michael Wiss'
+  });

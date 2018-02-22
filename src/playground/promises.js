9997190f5128ea300c43a2b3aa8c4efebc9d6922
MichaelWiss <michael.wiss@gmail.com>
2018-02-22 11:35:06 -0600

    promises

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,3 +1,16 @@
-const promise = new Promise(() => {
-   
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('This is my resolved data');
+		resolve('This is my other resloved data');
+	}, 5000);
 });
+
+console.log('before');
+
+promise.then((data) => {
+	console.log('1', data);
+});
+
+
+
+console.log('after');

07d15c9d197613106d6d380d1cbd6d4340f79075
MichaelWiss <michael.wiss@gmail.com>
2018-03-08 14:22:05 -0600

    expense action testing

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -93,16 +93,4 @@ test('should add expense with defaults to database and store', (done) => {
   });
 });
 
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       id: expect.any(String),
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0
-//     }
-//   });
-// });
+test('should setup st expense action object with data',)
